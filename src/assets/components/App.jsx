fe9d8753c4c88aeeef718df0c53cd3ee9721ed4a
jagadeesh0004 <jagadeeswarganisetti@gmail.com>
2025-11-15 10:35:18 +0530

    add particle bg

--- src/assets/components/App.jsx
+++ src/assets/components/App.jsx
@@ -5,16 +5,45 @@ import About from "./About";
 import Skills from "./Skills";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import Particles from "./Particles";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <div id="home"><Content /></div>
-      <div id="about"><About /></div>
-      <div id="skills"><Skills /></div>
-      <div id="projects"><Projects /></div>
-      <div id="contact"><Contact /></div>
+    <div
+      style={{
+        width: "100vw",
+        minHeight: "100vh",
+        position: "relative",
+        backgroundColor: "#000",
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          zIndex: 10,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Navbar />
+
+        <div id="home"><Content /></div>
+        <div id="about"><About /></div>
+        <div id="skills"><Skills /></div>
+        <div id="projects"><Projects /></div>
+        <div id="contact"><Contact /></div>
+      </div >
+
+      <Particles
+  particleColors={["#ffffff", "#9BE5FF"]}
+  particleCount={300}
+  particleSpread={10}
+  speed={0.08}
+  particleBaseSize={50}
+  alphaParticles={true}
+  disableRotation={true}
+/>
+
     </div>
   );
 };
